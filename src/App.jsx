fcd6749af48c244e782f92c0fcadd3e4c903415d
Jett Anderson <jett.r.anderson@gmail.com>
2025-12-07 22:50:59 -0600

    objects in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-const title = 'React!';
+const welcome = {
+  greeting: 'Hey',
+  title: 'React',
+}
+
+function getTitle(title) {
+  return title;
+}
 
 function App() {
   // you can do something in between
 
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>{welcome.greeting} {getTitle('World')}</h1>
       <label htmlFor="search">Search:
         <input id="search" type="text" />
       </label>
