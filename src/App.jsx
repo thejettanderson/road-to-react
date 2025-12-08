15246ce07421426bc4bbc540689451fcdd3f4f2b
Jett Anderson <jett.r.anderson@gmail.com>
2025-12-07 23:07:30 -0600

    array mapping and lists in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,48 @@
-const welcome = {
-  greeting: 'Hey',
-  title: 'React',
-}
-
-function getTitle(title) {
-  return title;
-}
+const list = [
+  {
+    title: 'React',
+    url: 'https://react.dev/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+]
 
 function App() {
   // you can do something in between
 
   return (
     <div>
-      <h1>{welcome.greeting} {getTitle('World')}</h1>
+      <h1>My Hacker Stories</h1>
       <label htmlFor="search">Search:
         <input id="search" type="text" />
       </label>
-      
+
+      <hr />
+
+      <ul>
+        {list.map(item => {
+          return (
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
